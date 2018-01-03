a18f787ddd903c63178a4548e66e432419324071
lapots <sebastrident@gmail.com>
2018-01-03 16:14:53 +0300

    Application template

---
+++ src/main/java/com/lapots/breed/editor/domain/PlayerCharacter.java
@@ -0,0 +1,8 @@
+package com.lapots.breed.editor.domain;
+
+import lombok.Data;
+
+@Data
+public class PlayerCharacter {
+    private String name;
+}
--- src/main/java/com/lapots/breed/editor/fx/ApplicationController.java
+++ src/main/java/com/lapots/breed/editor/fx/ApplicationController.java
@@ -1,17 +1,26 @@
 package com.lapots.breed.editor.fx;
 
+import com.lapots.breed.editor.domain.PlayerCharacter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 
 public class ApplicationController {
+    @FXML
+    private TextField character_name;
 
     @FXML
-    private void handleClickMe(ActionEvent event) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Button clicked");
-        alert.setContentText("You clicked the button!");
-        alert.setHeaderText("");
-        alert.showAndWait();
+    private void handleCharacterCreation(ActionEvent event) {
+        PlayerCharacter character = createCharacter();
+    }
+
+    private PlayerCharacter createCharacter() {
+        PlayerCharacter character = new PlayerCharacter();
+        character.setName(blankIfNull(character_name.getText()));
+        return character;
+    }
+
+    private String blankIfNull(String field) {
+        return field == null? "" : field;
     }
 }
