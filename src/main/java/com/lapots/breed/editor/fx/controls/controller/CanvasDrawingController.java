f1b5b97f71f8a8a4540e0875bd9c399bb848eca9
lapots <sebastrident@gmail.com>
2018-01-07 14:03:05 +0300

    Basic layer support

--- src/main/java/com/lapots/breed/editor/fx/controls/controller/CanvasDrawingController.java
+++ src/main/java/com/lapots/breed/editor/fx/controls/controller/CanvasDrawingController.java
@@ -1,12 +1,20 @@
 package com.lapots.breed.editor.fx.controls.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ComboBox;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class CanvasDrawingController implements Initializable {
@@ -16,12 +24,46 @@ public class CanvasDrawingController implements Initializable {
 
     @FXML
     private Canvas character_canvas;
-    private GraphicsContext gc;
+
+    @FXML
+    private ComboBox<String> layersBox;
+
+    private List<Canvas> layers = new ArrayList<>();
+    private ObservableList<String> layersList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        gc = character_canvas.getGraphicsContext2D();
+        GraphicsContext gc = character_canvas.getGraphicsContext2D();
         gc.setFill(Color.WHITE);
         gc.fillRect(0, 0, character_canvas.getWidth(), character_canvas.getHeight());
+        layers.add(character_canvas);
+
+        layersList.add("layer " + layers.size());
+        layersBox.setItems(layersList);
+        layersBox.getSelectionModel().selectLast();
+        layersBox.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
+            layers.get(newValue.intValue()).toFront();
+            System.out.println("Current layer: " + (newValue.intValue() + 1));
+        });
+    }
+
+    @FXML
+    private void handleAddLayer(ActionEvent event) {
+        addLayer();
+        layersBox.getSelectionModel().selectLast();
+    }
+
+    public int addLayer() {
+        Canvas layer = new Canvas(character_canvas.getWidth(), character_canvas.getHeight());
+        layers.add(layer);
+        layersList.add("layer " + layers.size());
+        return layers.size();
+    }
+
+    public GraphicsContext getLayer(int layer) {
+        if (layer < 0 || layer > layers.size()) {
+            return null;
+        }
+        return layers.get(layer).getGraphicsContext2D();
     }
 }
